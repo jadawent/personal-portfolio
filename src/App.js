d15350b9391bbc4f59350ce16809ea54124cff27
jadawent <jadawent@gmail.com>
2025-05-20 15:06:54 -0500

    added nav bar and sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import gradPhoto from './thumbnail_IMG_8453.jpg';
+import building from './building-columns-solid.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <a href="#about" className="nav-link">About Me</a>
+        <a href="#projects" className="nav-link">My Projects</a>
+        <a href="#contact" className="nav-link">Contact Me!</a>
+      </nav>
+
+      <div className="App">
+        <section id="about" className="aboutSection">
+            <img className="image-size" src={gradPhoto} alt="" />
+            <div className="text">
+                <h2 className="Name"> Jada Wentland </h2>
+                <h2 className="intro">
+                    Hey there! I am a recent graduate in Computer Science with a strong foundation in software
+                    development, problem solving, and a passion for learning new technologies. I'm excited to
+                    begin my journey in the tech industry, contribute to meaningful projects, and grow as a developer.
+                </h2>
+            </div>
+        </section>
+        <div>
+            <h2 className="education"> My Education </h2>
+            <img className="schoolIcon" src={building} alt="" />
+        </div>
+
+        <h2 className="tools"> Essential Tools I Use </h2>
+
+        <section id="projects" className="projectSection">
+          <h2 className="projectHeader"> My Projects</h2>
+          <p></p>
+        </section>
+
+        <section id="contact" className="contactSection">
+          <h2 className="contactHeader">Contact Me!</h2>
+          <p></p>
+        </section>
+      </div>
+    </>
   );
 }
 
