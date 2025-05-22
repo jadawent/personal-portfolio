7903473706bfe312637bf96b99465ef54ff6b777
jadawent <jadawent@gmail.com>
2025-05-22 15:47:28 -0500

    added education, tools, contact section and began on projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import gradPhoto from './thumbnail_IMG_8453.jpg';
-import building from './building-columns-solid.svg';
+import cssLogo from './csslogo.svg';
+import githubLogo from './github.svg';
+import javaLogo from './java.svg';
+import mysqlLogo from './mysql.svg';
+import reactLogo from './react.svg';
+import pythonLogo from './python.svg';
+import springBootLogo from './spring-boot.svg';
+import workflow from './workflow.png';
+
 
 function App() {
   return (
@@ -25,19 +33,69 @@ function App() {
         </section>
         <div>
             <h2 className="education"> My Education </h2>
-            <img className="schoolIcon" src={building} alt="" />
+                <p className="educationText">
+                    🎓 Bachelors of Science in Computer Science
+                </p>
+                <p className="educationText">
+                    🏛️ Metropolitan State University, Class of 2025
+                </p>
         </div>
 
         <h2 className="tools"> Essential Tools I Use </h2>
+        <div className="logoSection">
+            <div className="logoContainer">
+                <img className="logos" src={javaLogo} alt="Java" />
+                <p className="logoLabel"> Java </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={springBootLogo} alt="SpringBoot" />
+                <p className="logoLabel"> Spring Boot </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={pythonLogo} alt="Python" />
+                <p className="logoLabel"> Python </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={reactLogo} alt="React" />
+                <p className="logoLabel"> React </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={cssLogo} alt="CSS" />
+                <p className="logoLabel"> CSS </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={mysqlLogo} alt="MySQL" />
+                <p className="logoLabel"> MySQL </p>
+            </div>
+            <div className="logoContainer">
+                <img className="logos" src={githubLogo} alt="GitHub" />
+                <p className="logoLabel"> GitHub </p>
+            </div>
+        </div>
 
         <section id="projects" className="projectSection">
           <h2 className="projectHeader"> My Projects</h2>
-          <p></p>
+          <div className="gitHub">
+              <a href="https://www.github.com/jadawent" target="_blank" rel="noopener noreferrer">
+                      Check out my projects on my GitHub!
+              </a>
+          </div>
+          <div className="projectContainer">
+            <img className="workflow" src={workflow} alt="workflow" />
+            <p className="workflowText"> WorkFlow. A web application to streamline task management
+             and communication within restaurants. Built using React, CSS, Java Spring Boot and MySQL.</p>
+          </div>
         </section>
 
         <section id="contact" className="contactSection">
           <h2 className="contactHeader">Contact Me!</h2>
-          <p></p>
+          <div className="linkedIn">
+              <a href="https://www.linkedin.com/in/jada-wentland" target="_blank" rel="noopener noreferrer">
+                Connect with me on LinkedIn
+              </a>
+              <p> or </p>
+              <a href="mailto:jadawent@gmail.com"> Email Me! </a>
+          </div>
         </section>
       </div>
     </>
